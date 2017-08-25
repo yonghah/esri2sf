45af2a641cb8abfdb1c0c7f2dd7934563e6c6f39
Yongha Hwang <yongha.hwang@gmail.com>
2017-08-25 11:44:55 -0400

    revert sapply

--- R/esri2sf.R
+++ R/esri2sf.R
@@ -79,8 +79,7 @@ esri2sfGeom <- function(jsonFeats, geomType) {
     geoms <- esri2sfPolyline(jsonFeats)
   }
   # attributes
-  atts <- lapply(jsonFeats, '[[', 1) %>%
-          sapply(function(x) ifelse(x=="NULL", NA, x))
+  atts <- lapply(jsonFeats, '[[', 1)
   af <- dplyr::bind_rows(lapply(atts, as.data.frame.list, stringsAsFactors=FALSE))
   # geometry + attributes
   df <- sf::st_sf(geoms, af, geom=geoms, crs="+init=epsg:4326")

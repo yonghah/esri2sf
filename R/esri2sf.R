de80f5d0e5fd5882f3412354339869c5488cd649
Eli Pousson <eli.pousson@gmail.com>
2020-12-17 01:14:34 -0500

    Add bbox parameter to esri2sf function

--- R/esri2sf.R
+++ R/esri2sf.R
@@ -7,8 +7,8 @@
 #' @param where string for where condition. Default is `1=1` for all rows.
 #' @param token string for authentication token (if needed).
 #' @param geomType string specifying the layer geometry ('esriGeometryPolygon' or 'esriGeometryPoint' or 'esriGeometryPolyline' - if `NULL`, will try to be inferred from the server)
-#' @param crs coordinates reference system (see [sf::st_sf()]).
-#'
+#' @param crs coordinate reference system (see [sf::st_sf()]).
+#' @param bbox bbox class object from [sf::st_bbox()].
 #' @param ... additional named parameters to pass to the query. ex) "resultRecordCount = 3"
 #' @return sf dataframe (`esri2sf`) or tibble dataframe (`esri2sf`).
 #'
@@ -20,32 +20,47 @@
 #' #' The list of layers and their respective id numbers can be found by viewing
 #' the service's url in a web browser and viewing the "Layers" heading (e.g.,
 #' <https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/#mapLayerList>).
-#' 
-#' @examples 
+#'
+#' @examples
 #' baseURL <- "https://sampleserver1.arcgisonline.com/ArcGIS/rest/"
 #' url <- paste0(baseURL, "services/Demographics/ESRI_Census_USA/MapServer/3")
-#' outFields <- c("POP2007", "POP2000") 
+#' outFields <- c("POP2007", "POP2000")
 #' where <- "STATE_NAME = 'Michigan'"
-#' df <- esri2sf(url, outFields = outFields, where = where) 
-#' plot(df) 
+#' df <- esri2sf(url, outFields = outFields, where = where)
+#' plot(df)
 #'
 #' @export
 
-esri2sf <- function(url, outFields = c("*"), where = "1=1", token = "", 
-  geomType = NULL, crs = 4326, ...) {
-  layerInfo <- jsonlite::fromJSON(content(POST(url, query = list(f = "json", 
-    token = token), encode = "form", config = config(ssl_verifypeer = FALSE)), 
-    as = "text"))
+esri2sf <- function(url, outFields = c("*"), where = "1=1", bbox = NULL, token = "",
+                    geomType = NULL, crs = 4326, ...) {
+  layerInfo <- jsonlite::fromJSON(content(POST(url, query = list(f = "json",
+                                                                 token = token), encode = "form", config = config(ssl_verifypeer = FALSE)),
+                                          as = "text"))
   print(layerInfo$type)
   if (is.null(geomType)) {
-    if (is.null(layerInfo$geometryType)) 
+    if (is.null(layerInfo$geometryType))
       stop("geomType is NULL and layer geometry type ('esriGeometryPolygon' or 'esriGeometryPoint' or 'esriGeometryPolyline') could not be inferred from server.")
-    
+
     geomType <- layerInfo$geometryType
   }
+
   print(geomType)
+
+  if (!is.null(layerInfo$extent$spatialReference$latestWkid))
+    print(paste0("Coordinate Reference System: ", layerInfo$extent$spatialReference$latestWkid))
+
+  if (class(bbox) == "bbox") {
+    if ((sf::st_crs(bbox)$input != layerInfo$extent$spatialReference$latestWkid) && !is.null(layerInfo$extent$spatialReference$latestWkid)) {
+      bbox <- sf::st_bbox(sf::st_transform(sf::st_as_sfc(bbox), layerInfo$extent$spatialReference$latestWkid))
+    }
+  } else if (!is.null(bbox)) {
+    stop("The provided bbox must be a class bbox object.")
+  }
+
+  bbox <- paste0(unlist(as.list(bbox), use.names=FALSE), collapse = ",")
+
   queryUrl <- paste(url, "query", sep = "/")
-  esriFeatures <- getEsriFeatures(queryUrl, outFields, where, token, ...)
+  esriFeatures <- getEsriFeatures(queryUrl, outFields, where, bbox, token, ...)
   esri2sfGeom(esriFeatures, geomType, crs)
 }
 
@@ -53,15 +68,15 @@ esri2sf <- function(url, outFields = c("*"), where = "1=1", token = "",
 #' @export
 esri2df <- function(url, outFields = c("*"), where = "1=1", token = "", ...) {
   layerInfo <- fromJSON(content(
-    POST(url, 
-        query = list(f = "json", token = token), 
-        encode = "form", 
-        config = config(ssl_verifypeer = FALSE)
+    POST(url,
+         query = list(f = "json", token = token),
+         encode = "form",
+         config = config(ssl_verifypeer = FALSE)
     ), as = "text"))
-  
+
   print(layerInfo$type)
   if (layerInfo$type != "Table") stop("Layer type for URL is not 'Table'.")
-  
+
   queryUrl <- paste(url, "query", sep = "/")
   esriFeatures <- getEsriFeatures(queryUrl, outFields, where, token)  #, ...)
   getEsriTable(esriFeatures)

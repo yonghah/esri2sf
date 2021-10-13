674cfe0855b27c2d62971cefe854e23a4c16d2b3
jacpete <jacobmpeterson12@gmail.com>
2021-10-13 01:36:22 -0500

    add getAllLayersAndTables function

---
+++ R/getAllLayersAndTables.R
@@ -0,0 +1,29 @@
+#' @title Get All Layers and Tables
+#'
+#' @description Retrieve JSON for all layers and tables as specified in
+#' [https://si-pweb-vecmap.si.edu/vectormap/sdk/rest/index.html#/All_Layers_and_Tables/02ss0000005t000000/](https://si-pweb-vecmap.si.edu/vectormap/sdk/rest/index.html#/All_Layers_and_Tables/02ss0000005t000000/).
+#' Performs a POST request towards the server url in one of the following the
+#' forms:
+#' * `https://<host>/<instance>/rest/services/<folderName>/serviceName>/MapServer/layers`
+#' * `https://<host>/<instance>/rest/services/serviceName>/MapServer/layers`
+#' * `https://<host>/<instance>/rest/services/<folderName>/serviceName>/FeatureServer/layers`
+#' * `https://<host>/<instance>/rest/services/serviceName>/FeatureServer/layers`
+#'
+#' @param urlServer The url for the Map/Feature server. Ends in 'MapServer' or
+#'   'FeatureServer'.
+#'
+#' @md
+#'
+#' @return A list from the JSON return.
+#' @export
+getAllLayersAndTables <- function(urlServer) {
+  query <- list(
+    f = "json",
+    returnDomainNames = TRUE
+  )
+
+  r <- POST(paste(urlServer, "layers", sep = "/"),
+            body = query, encode = "form")
+  response <- fromJSON(content(r, "text"))
+  return(response)
+}

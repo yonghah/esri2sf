d803d9a07d1b5bf9b59553e77c018a09c3032a0e
jacpete <jpeterson@marc.org>
2021-10-14 09:18:49 -0500

    add stats to dependencies

--- R/addDomainInfo.R
+++ R/addDomainInfo.R
@@ -41,14 +41,14 @@ addDomainInfo <- function(df, url, token = "") {
 
   #Check handled domain types
   handledDomainTypes <- c('codedValue', 'range')
-  domainTypes <- na.omit(layerTableFields[['domain_type']])
+  domainTypes <- stats::na.omit(layerTableFields[['domain_type']])
   if (!all(domainTypes %in% handledDomainTypes)) {
     newDomainTypes <- domainTypes[!(domainTypes %in% handledDomainTypes)]
     stop(paste0("Field domain of type(s): ", paste0("'", newDomainTypes, "'", collapse = ", "), " found in the function esri2sf:::getDomainInfo(). Please start an issue at 'https://github.com/yonghah/esri2sf/issues/new/choose' so that the novel domain type can be handled by the package."))
   }
 
 
-  if (length(na.omit(layerTableFields[['domain_type']])) == 0) {
+  if (length(stats::na.omit(layerTableFields[['domain_type']])) == 0) {
     return(df)
   } else {
     #replace values in df with domain information (codedValue domains)

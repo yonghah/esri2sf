8bc6579716c99b6d500afa29ee38b22cfa0d573d
jacpete <jpeterson@marc.org>
2021-10-14 09:03:17 -0500

    fix domain check taht wasn't properly handling when the domain was logical

--- R/addDomainInfo.R
+++ R/addDomainInfo.R
@@ -19,8 +19,13 @@ addDomainInfo <- function(df, url, token = "") {
     return(df)
   }
 
+  #Check that domain column is logical (all NA so domains to handle)
+  if ('logical' %in% class(layerTableFields[['domain']])) {
+    return(df)
+  }
+
   #Check that domain column is a dataframe
-  if (!('data.frame' %in% class(layerTableFields['domain']))) {
+  if (!('data.frame' %in% class(layerTableFields[['domain']]))) {
     stop("The domain field in the layerTableFields is not a dataframe. Edits need to be made to the addDomainInfo() function. Please start an issue at 'https://github.com/yonghah/esri2sf/issues/new/choose' so that the issue can be fixed.")
   }
 

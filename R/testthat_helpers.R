42f3dbb99d95f2a5af6f0f7dc204c4d3b124e6b4
jacpete <jacobmpeterson12@gmail.com>
2021-11-02 12:46:46 -0500

    add namespace for testthat functions

--- R/testthat_helpers.R
+++ R/testthat_helpers.R
@@ -1,8 +1,8 @@
 skip_if_offline_url <- function(url) {
-  skip_if_offline()
+  testthat::skip_if_offline()
   response <- httr::GET(url)
   if (!httr::http_error(response) & httr::content(response, as = 'text') != "") {
     return(invisible(TRUE))
   }
-  skip(paste0(url, " could not be resolved."))
+  testthat::skip(paste0(url, " could not be resolved."))
 }

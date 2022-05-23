a88e56464fdfcd4bcf01faa4208d53a7eaadb6e6
Eli Pousson <eli.pousson@gmail.com>
2022-05-22 23:27:04 -0400

    refactor: switch esriLayers to use esriRequest
    - refactor: switch esriLayers to use esriRequest (replacing httr w/ httr2)
    - refactor: add is_urlServer helper function

--- R/esriLayers.R
+++ R/esriLayers.R
@@ -3,35 +3,54 @@
 #' @description Retrieve JSON for all layers and tables as specified in
 #' [https://si-pweb-vecmap.si.edu/vectormap/sdk/rest/index.html#/All_Layers_and_Tables/02ss0000005t000000/](https://si-pweb-vecmap.si.edu/vectormap/sdk/rest/index.html#/All_Layers_and_Tables/02ss0000005t000000/).
 #' Service type must either be a MapServer or FeatureServer.
-#' Performs a POST request towards the server url in one of the following the
+#' Performs a request towards the server url in one of the following the
 #' forms:
 #' * `https://<host>/<instance>/rest/services/<folderName>/serviceName>/MapServer/layers`
 #' * `https://<host>/<instance>/rest/services/serviceName>/MapServer/layers`
 #' * `https://<host>/<instance>/rest/services/<folderName>/serviceName>/FeatureServer/layers`
 #' * `https://<host>/<instance>/rest/services/serviceName>/FeatureServer/layers`
 #'
+#' @inheritParams esriRequest
 #' @param url The url for the Map/Feature server. If given a url specifying a
 #' layer or table ID it will truncate it.
-#' @param token String for authentication token (if needed).
-#'
+#' @param returnDomainNames If `TRUE`, the REST API response does not include
+#'   the full domain information for each layer (only the domain names). Defaults to `TRUE`.
+#' @param returnUpdates If `TRUE`, updated features will be returned; defaults to `NULL`.
+#' @param ... Additional parameters passed to
 #' @return A list from the JSON return.
 #' @export
-esriLayers <- function(url, token = "") {
-  #Format url (remove layer.table ID and check it is valid)
+esriLayers <- function(url, token = NULL, returnUpdates = NULL, returnDomainNames = TRUE, ...) {
+  # Format url (remove layer.table ID and check it is valid)
   urlServer <- esriUrl_serviceUrl(url, token = token)
 
-  if (!grepl("/(FeatureServer|MapServer)/?$", urlServer)) {
-    stop("Url is not valid.\n Service type is not either 'MapServer' or 'FeatureServer'")
+  is_urlServer(urlServer)
+
+  resp <-
+    esriRequest(
+      url = url,
+      format = "json",
+      token = token,
+      returnDomainNames = returnDomainNames,
+      returnUpdates = returnUpdates
+    )
+
+  resp <- httr2::resp_body_json(resp = resp, check_type = FALSE, ...)
+
+  return(resp)
+}
+
+#' Is URL a FeatureServer or MapServer URL?
+#'
+#' @noRd
+#' @importFrom cli cli_abort
+is_urlServer <- function(url) {
+  if (!grepl("/(FeatureServer|MapServer)/?$", url)) {
+    cli::cli_abort(
+      c(
+        "Invalid {.arg url}:",
+        "{.url {url}}",
+        "Function requires a {.val MapServer} or {.val FeaturepServer} url."
+      )
+    )
   }
-
-  query <- list(
-    f = "json",
-    token = token,
-    returnDomainNames = TRUE
-  )
-
-  r <- httr::POST(paste(urlServer, "layers", sep = "/"),
-            body = query, encode = "form")
-  response <- jsonlite::fromJSON(httr::content(r, "text"))
-  return(response)
 }

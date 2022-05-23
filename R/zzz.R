dc3c2bda2311b6b83dc975ca37bd1be633235d26
Eli Pousson <eli.pousson@gmail.com>
2022-05-22 23:37:43 -0400

    refactor: update functions to work w/ httr2 and cli

--- R/zzz.R
+++ R/zzz.R
@@ -1,175 +1,182 @@
+#' Get object ids
+#'
+#' @noRd
+getObjectIds <- function(url,
+                         where = "1=1",
+                         token = NULL,
+                         geometry = NULL,
+                         geometryType = NULL,
+                         ...) {
+  resp <-
+    esriRequest(
+      url = url,
+      append = "query",
+      token = token,
+      format = "json",
+      where = where,
+      geometryType = geometryType,
+      geometry = geometry,
+      returnIdsOnly = TRUE,
+      ...
+    )
 
-getObjectIds <- function(queryUrl, where = "1=1", bbox = NULL, token = "", ...) {
+  resp <- httr2::resp_body_json(resp = resp, check_type = FALSE)
 
-  # create Simple Features from ArcGIS servers json response
-  query <- list(where = where, geometryType = "esriGeometryEnvelope",
-                geometry = bbox, returnIdsOnly = "true", token = token,
-                f = "json", ...)
-  responseRaw <- httr::content(httr::POST(queryUrl, body = query, encode = "form",
-                              config = httr::config(ssl_verifypeer = FALSE)), as = "text")
-
-  response <- jsonlite::fromJSON(responseRaw)
-  response$objectIds
+  return(resp$objectIds)
 }
 
-getMaxRecordsCount <- function(queryUrl, token = "", upperLimit = FALSE) {
+#' Get count of maximum records per request
+#'
+#' @noRd
+getMaxRecordsCount <- function(url,
+                               token = "",
+                               upperLimit = FALSE) {
+  urlInfo <- esriCatalog(url)
 
-  url <- sub("/query$", "", queryUrl)
-
-  query <- list(f = "json", token = token)
-
-  responseRaw <- httr::content(httr::POST(url, body = query, encode = "form",
-                              config = httr::config(ssl_verifypeer = FALSE)), as = "text")
-  response <- jsonlite::fromJSON(responseRaw)
-
-  if (!is.null(response[['maxRecordCount']])) {
-    if (response[['maxRecordCount']] > 25000 & upperLimit) {
+  if (!is.null(urlInfo[["maxRecordCount"]])) {
+    if (urlInfo[["maxRecordCount"]] > 25000 & upperLimit) {
       maxRC <- 25000L
     } else {
-      maxRC <- response[['maxRecordCount']]
+      maxRC <- urlInfo[["maxRecordCount"]]
     }
   } else {
     maxRC <- 500L
   }
 
-  maxRC
-
+  return(maxRC)
 }
 
-getRecordsCount <- function(queryUrl, where = "1=1", token = "", ...) {
-
-  query <- list(where = where, returnCountOnly = 'true', token = token, f = "json", ...)
-
-  responseRaw <- httr::content(httr::POST(queryUrl, body = query, encode = "form",
-                              config = httr::config(ssl_verifypeer = FALSE)), as = "text")
-  response <- jsonlite::fromJSON(responseRaw)
-
-  response[['count']]
-
-}
-
+#' Get table for Table layer
+#'
+#' @noRd
 getEsriTable <- function(jsonFeats) {
-  atts <- lapply(lapply(jsonFeats, `[[`, 1),
-                 function(att) lapply(att, function(x) ifelse(is.null(x), NA, x)))
+  atts <- lapply(
+    lapply(jsonFeats, `[[`, 1),
+    function(att) lapply(att, function(x) ifelse(is.null(x), NA, x))
+  )
   df <- dplyr::bind_rows(lapply(atts, as.data.frame.list, stringsAsFactors = FALSE))
-  dplyr::as_tibble(df)
+
+  return(dplyr::as_tibble(df))
 }
 
 
-getEsriFeaturesByIds <- function(ids, queryUrl, fields = c("*"), token = "", crs = 4326, ...) {
+#' Get features using feature ids from getObjectIds
+#'
+#' @noRd
+getEsriFeaturesByIds <- function(ids,
+                                 url,
+                                 fields = c("*"),
+                                 token = NULL,
+                                 crs = 4326,
+                                 ...) {
+
   # create Simple Features from ArcGIS servers json response
-  query <- list(objectIds = paste(ids, collapse = ","),
-                outFields = paste(fields, collapse = ","),
-                token = token, outSR = crs, f = "json", ...)
+  resp <-
+    esriRequest(
+      url,
+      append = "query",
+      format = "json",
+      token = token,
+      objectIds = paste(ids, collapse = ","),
+      outFields = paste(fields, collapse = ","),
+      outSR = crs,
+      ...
+    )
 
-  responseRaw <- httr::content(httr::POST(queryUrl, body = query, encode = "form",
-                              config = httr::config(ssl_verifypeer = FALSE)), as = "text")
+  resp <-
+    httr2::resp_body_json(
+      resp,
+      check_type = FALSE,
+      # Additional parameters passed to jsonlite::fromJSON
+      simplifyDataFrame = FALSE,
+      simplifyVector = FALSE,
+      digits = NA
+    )
 
-  response <- jsonlite::fromJSON(responseRaw, simplifyDataFrame = FALSE, simplifyVector = FALSE,
-                       digits = NA)
-
-  if ('error' %in% names(response)) {
-    stop(paste0("Error code: ", response$error$code, ".\n  Message: ", response$error$message, "\n  Details: ", response$error$details))
+  if ("error" %in% names(resp)) {
+    cli::cli_abort(
+      c(
+        "Error code: {resp$error$code}.",
+        "Message: {resp$error$message}",
+        "Details: {resp$error$details}"
+      )
+    )
   }
 
-  response$features
+  return(resp$features)
 }
 
-esri2sfPoint <- function(features) {
-  getPointGeometry <- function(feature) {
-    if (is.numeric(unlist(feature$geometry))) {
-      sf::st_point(unlist(feature$geometry))
-    } else sf::st_point()
-  }
-  sf::st_sfc(lapply(features, getPointGeometry))
-}
 
-esri2sfPolygon <- function(features) {
-  ring2matrix <- function(ring) do.call(rbind, lapply(ring, unlist))
-  rings2multipoly <- function(rings)
-    sf::st_multipolygon(list(lapply(rings, ring2matrix)))
+#' Get ESRI features
+#'
+#' @noRd
+getEsriFeatures <- function(url,
+                            fields = c("*"),
+                            where = "1=1",
+                            geometry = NULL,
+                            geometryType = NULL,
+                            token = NULL,
+                            crs = 4326,
+                            progress = FALSE,
+                            ...) {
+  ids <-
+    getObjectIds(
+      url = url,
+      where = where,
+      geometry = geometry,
+      geometryType = geometryType,
+      token = token,
+      ...
+    )
 
-  getGeometry <- function(feature) {
-    if (is.null(unlist(feature$geometry$rings))) {
-      sf::st_multipolygon()
-    } else rings2multipoly(feature$geometry$rings)
-  }
-
-  sf::st_sfc(lapply(features, getGeometry))
-}
-
-esri2sfPolyline <- function(features) {
-  path2matrix <- function(path) do.call(rbind, lapply(path, unlist))
-  paths2multiline <- function(paths) sf::st_multilinestring(lapply(paths, path2matrix))
-
-  getGeometry <- function(feature) paths2multiline(feature$geometry$paths)
-
-  sf::st_sfc(lapply(features, getGeometry))
-}
-
-getEsriFeatures <- function(queryUrl, fields = c("*"), where = "1=1", bbox = NULL, token = "", crs = 4326, progress = FALSE, ...) {
-  ids <- getObjectIds(queryUrl, where, bbox, token, ...)
   if (is.null(ids)) {
-    warning("No records match the search criteria.")
+    cli::cli_alert_danger("No records match the search criteria.")
     return()
   }
-  maxRC <- getMaxRecordsCount(queryUrl, token, upperLimit = TRUE)
+  maxRC <- getMaxRecordsCount(url, token, upperLimit = TRUE)
   idSplits <- split(ids, seq_along(ids) %/% maxRC)
 
-  if (is.null(crs)) {
-    crs <- ""
-  } else if (is.numeric(crs)) {
-    crs <- as.character(crs)
-  } else if (isWktID(crs)) {
-    crs <- sub(pattern = "^(EPSG|ESRI):", replacement = "", x = crs)
-  } else {
-    crs <- jsonlite::toJSON(list("wkt" = WKTunPretty(sf::st_crs(crs)$WKT1_ESRI)), auto_unbox=TRUE)
-  }
+  crs <-
+    dplyr::case_when(
+      is.null(crs) ~ "",
+      is.numeric(crs) ~ as.character(crs),
+      isWktID(crs) ~ sub(pattern = "^(EPSG|ESRI):", replacement = "", x = crs),
+      TRUE ~ as.character(jsonlite::toJSON(list("wkt" = WKTunPretty(sf::st_crs(crs)$WKT1_ESRI)), auto_unbox = TRUE))
+    )
 
-  #Check if pbapply progress bar can be used
+  # Check if pbapply progress bar can be used
   if (!requireNamespace("pbapply", quietly = TRUE) & progress) {
-    warning("Package \"pbapply\" needed to use the progress argument. Please install it. Setting `progress` to FALSE.",
-         call. = FALSE)
+    cli::cli_alert_danger(
+      "The {.pkg pbapply} package is not installed.
+      {.pkg pbapply} is required to use the {.arg progress} argument.
+      Setting {.arg progress} to {.val FALSE}."
+    )
     progress <- FALSE
   }
 
   if (progress) {
-    results <- pbapply::pblapply(idSplits, getEsriFeaturesByIds, queryUrl, fields, token, crs, ...)
+    results <-
+      pbapply::pblapply(
+        idSplits,
+        getEsriFeaturesByIds,
+        url, fields, token, crs, ...
+      )
   } else {
-    results <- lapply(idSplits, getEsriFeaturesByIds, queryUrl, fields, token, crs, ...)
+    results <-
+      lapply(
+        idSplits,
+        getEsriFeaturesByIds,
+        url, fields, token, crs, ...
+      )
   }
 
-  unlist(results, recursive = FALSE)
+  return(unlist(results, recursive = FALSE))
 }
 
-esri2sfGeom <- function(jsonFeats, geomType, crs = 4326) {
-  # convert esri json to simple feature
-  geoms <- switch(geomType,
-                  esriGeometryPolygon = esri2sfPolygon(jsonFeats),
-                  esriGeometryPoint = esri2sfPoint(jsonFeats),
-                  esriGeometryPolyline = esri2sfPolyline(jsonFeats)
-  )
-
-
-  #Format CRS
-  if (isWktID(crs)) {
-    crs <- gsub(pattern = "^(EPSG|ESRI):", replacement = "", x = crs)
-    crs <- getWKTidAuthority(crs)
-  }
-
-
-  # attributes
-  atts <- lapply(lapply(jsonFeats, `[[`, 1),
-                 function(att) lapply(att, function(x) ifelse(is.null(x), NA, x)))
-
-  af <- dplyr::bind_rows(lapply(atts, as.data.frame.list, stringsAsFactors = FALSE))
-  # geometry + attributes
-  sf::st_sf(geoms, af, crs = crs)
-}
-
-
+#' Get authority string for WKT (well known text) id
+#'
+#' @noRd
 getWKTidAuthority <- function(wktID) {
-
   projPaths <- file.path(sf::sf_proj_search_paths(), "proj.db")
   projDB <- projPaths[file.exists(projPaths)][1]
 
@@ -180,27 +187,27 @@ getWKTidAuthority <- function(wktID) {
   DBI::dbDisconnect(con)
 
   if (nrow(crsData) == 0) {
-    stop(paste0("WKTid: ", wktID, " not found in proj.db"))
+    cli::cli_abort("WKTid: {.val {wktID}} not found in {.file proj.db}")
   }
 
   if (nrow(crsData) > 1) {
-    stop(paste0("WKTid: ", wktID, " has multiple entries in proj.db. Please specify authority (EPSG|ESRI) in crs argument"))
+    cli::cli_abort(
+      "WKTid: {.val {wktID}} has multiple entries in  {.file proj.db}.
+    Please specify authority (EPSG|ESRI) in {.arg crs} argument"
+    )
   }
 
   wktID <- paste0(crsData$auth_name, ":", crsData$code)
 
-  wktID
-
+  return(wktID)
 }
 
 isWktID <- function(crs) {
-
   is.numeric(crs) || grepl(pattern = "^(EPSG|ESRI):[[:digit:]]+$", x = crs)
-
 }
 
 WKTunPretty <- function(wkt) {
-
   gsub("\\n[[:blank:]]*", "", wkt)
-
 }
+
+utils::globalVariables(c("name", "serviceType", "type", "urlType"))
